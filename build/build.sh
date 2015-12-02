1d20fc0f7776a953dc56a31c2a4719f9c5e6b6a4
CoreOS Admin <jksun@thoughtworks.com>
2015-12-02 01:34:51 +0000

    launch mysql

--- build/build.sh
+++ build/build.sh
@@ -2,6 +2,8 @@
 cd /tmp/repo/app
 echo `pwd`
 ls
+mysql=$(docker run -d -p 3306:3306 -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=ke_tsu -e MYSQL_ROOT_PASSWORD=mysql 172.17.8.101:5000/mysql)
+gradle fC fM
 gradle test
 gradle standaloneJar
 cat > /tmp/repo/Dockerfile << EOF
