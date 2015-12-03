33fdbfad142bc010ce5a55668a0ec36d7de7fda9
CoreOS Admin <core@ip-10-21-2-213.us-west-1.compute.internal>
2015-12-03 17:02:28 +0000

    rebuild nginx

--- front/build.sh
+++ front/build.sh
@@ -4,9 +4,9 @@ npm install
 npm install -g webpack
 webpack
 cat > Dockerfile << EOF
-FROM nginx
-EXPOSE 8080
-ADD /dists /usr/share/nginx/html
+FROM 10.21.1.214:5000/nginx
+EXPOSE 80
+ADD /dist /usr/share/nginx/html
 EOF
 
 docker build -t $IMAGE /tmp/repo
