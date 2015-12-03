baeea91ab73bc218b51c17668d4b0547383cbe6d
CoreOS Admin <core@ip-10-21-2-213.us-west-1.compute.internal>
2015-12-03 19:04:45 +0000

    add run script

--- front/build.sh
+++ front/build.sh
@@ -3,10 +3,19 @@ cd /tmp/repo/
 npm install
 npm install -g webpack
 webpack
+
+cat > run.sh << EOF
+sed -i -e "s#{{API_PREFIX}}#\$API_PREFIX#g" /usr/share/nginx/html/bundle.js
+exec "$@"
+EOF
+
 cat > Dockerfile << EOF
 FROM 10.21.1.214:5000/nginx
 EXPOSE 80
+ADD run.sh run.sh
+RUN chmod +x run.sh
 ADD /dist /usr/share/nginx/html
+ENTRYPOINT ["./run.sh"]
 EOF
 
 docker build -t $IMAGE /tmp/repo
