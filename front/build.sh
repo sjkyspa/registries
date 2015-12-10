084aa52c1b0f18218003eb375e8478ea88787261
jksun <sjkyspa@gmail.com>
2015-12-10 17:12:08 +0800

    add info for front build

--- front/build.sh
+++ front/build.sh
@@ -1,8 +1,36 @@
 #!/bin/sh
 cd /tmp/repo/
-npm install
-npm install -g webpack
+
+puts_red() {
+    echo $'\033[0;31m'"      $@" $'\033[0m'
+}
+
+puts_red_f() {
+  while read data; do
+    echo $'\033[0;31m'"      $data" $'\033[0m'
+  done
+}
+
+puts_green() {
+  echo $'\033[0;32m'"      $@" $'\033[0m'
+}
+
+puts_step() {
+  echo $'\033[0;34m'" -----> $@" $'\033[0m'
+}
+
+
+puts_step "Staring install depends ..."
+npm install &> install.log
+if [ "$?" != "0" ]; then
+  puts_red "install depends failed"
+  cat install.log | puts_red_f
+fi
+puts_step "Install depends complete"
+
+puts_step "Start packing ..."
 webpack
+puts_step "Packing complete"
 
 cat > run.sh << EOF
 #!/bin/sh
@@ -20,4 +48,6 @@ ENTRYPOINT ["./run.sh"]
 CMD ["nginx", "-g", "daemon off;"]
 EOF
 
+puts_step "Start building app image ..."
 docker build -t $IMAGE /tmp/repo
+puts_step "Building app image complete"
