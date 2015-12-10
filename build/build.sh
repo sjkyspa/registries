38118087b68c4916c3e2992b4fa8a2c091eb5daa
jksun <sjkyspa@gmail.com>
2015-12-10 16:47:53 +0800

    reset after put color text

--- build/build.sh
+++ build/build.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 
 puts_red() {
-    echo $'\033[0;31m'"      $@"
+    echo $'\033[0;31m'"      $@" $'\033[0m'
 }
 
 puts_red_f() {
   while read data; do
-    echo $'\033[0;31m'"      $data"
+    echo $'\033[0;31m'"      $data" $'\033[0m'
   done
 }
 
 puts_green() {
-  echo $'\033[0;32m'"      $@"
+  echo $'\033[0;32m'"      $@" $'\033[0m'
 }
 
 puts_step() {
-  echo $'\033[0;34m'" -----> $@"
+  echo $'\033[0;34m'" -----> $@" $'\033[0m'
 }
 
 cd /tmp/repo/app
