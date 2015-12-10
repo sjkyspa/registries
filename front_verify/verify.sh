42031f3e4420d2903b01554710dda54ed39c4a2d
jksun <sjkyspa@gmail.com>
2015-12-10 17:23:04 +0800

    add front verify

---
+++ front_verify/verify.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+puts_red() {
+    echo $'\033[0;31m'"      $@" $'\033[0m'
+}
+
+puts_red_f() {
+  while read data; do
+    echo $'\033[0;31m'"      $data" $'\033[0m'
+  done
+}
+
+puts_green() {
+  echo $'\033[0;32m'"      $@" $'\033[0m'
+}
+
+puts_step() {
+  echo $'\033[0;34m'" -----> $@" $'\033[0m'
+}
+
+puts_step "Start verifying"
+sleep 20
+puts_step "Verify complete"
