7045087e9a64d72ac05293f89972ffe496917a75
jksun <sjkyspa@gmail.com>
2015-12-10 19:50:49 +0800

    skip verify when no manifest.json

--- verify/verify.sh
+++ verify/verify.sh
@@ -17,35 +17,40 @@ puts_green() {
 puts_step() {
   echo $'\033[0;34m'" -----> $@" $'\033[0m'
 }
+
+puts_step "Start to verify..."
+sleep 20
+puts_step "Verify success"
+
 cd /tmp/repo
+if [ -f "manifest.json" ]; then
+    puts_step "Start sync to ketsu"
+    first_commit=$(git log --reverse --pretty=format:%at |head -n1)
+    last_commit=$(git log --pretty=format:%at -n 1)
+    evaluation_duration=$(eval 'expr $last_commit - $first_commit')
+    puts_green "firstcommit $first_commit"
+    puts_green "lastcommit $last_commit"
+    puts_green "duration $evaluation_duration"
 
-first_commit=$(git log --reverse --pretty=format:%at |head -n1)
-last_commit=$(git log --pretty=format:%at -n 1)
-evaluation_duration=$(eval 'expr $last_commit - $first_commit')
-puts_green "firstcommit $first_commit"
-puts_green "lastcommit $last_commit"
-puts_green "duration $evaluation_duration"
+    evaluation_uri=$(cat manifest.json| jq -r '.evaluation_uri')
+    puts_green "evaluationuri $evaluation_uri"
+    if [ -n "$evaluation_uri" ] ; then
+        puts_red "missing manifest.json"
+        exit 1
+    fi
+    entry_point=$(echo $evaluation_uri | awk -F/ '{print $3}')
+    puts_green "entry point $entry_point"
 
+    if [ -n "$entry_point" ] ; then
+        puts_red "bad format of manifest.json"
+        exit 1
+    fi
 
-puts_step "Start sync to ketsu"
-evaluation_uri=$(cat manifest.json| jq -r '.evaluation_uri')
-puts_green "evaluationuri $evaluation_uri"
-if [ -n "$evaluation_uri" ] ; then
-    puts_red "missing manifest.json"
-    exit 1
+    curl -c cookie -b cookie "$entry_point/authentication" -d "user_name=bg"
+    curl -c cookie -b cookie "$entry_point/authentication" -d "user_name=bg"
+    result_status=$(curl -sSL --write-out "%{http_code}" -X POST -c cookie -b cookie $evaluation_uri -d "score=$evaluation_duration" -d "status=pass")
+    if [ "$result_status" != "200" ] ; then
+        puts_red "sync fail http code:$result_status"
+    fi
+    puts_step "Sync to ketsu complete"
 fi
-entry_point=$(echo $evaluation_uri | awk -F/ '{print $3}')
-puts_green "entry point $entry_point"
-
-if [ -n "$entry_point" ] ; then
-    puts_red "bad format of manifest.json"
-    exit 1
-fi
-
-curl -c cookie -b cookie "$entry_point/authentication" -d "user_name=bg"
-curl -c cookie -b cookie "$entry_point/authentication" -d "user_name=bg"
-result_status=$(curl -sSL --write-out "%{http_code}" -X POST -c cookie -b cookie $evaluation_uri -d "score=$evaluation_duration" -d "status=pass")
-if [ "$result_status" != "200" ] ; then
-    puts_red "sync fail http code:$result_status"
-fi
-puts_step "Sync to ketsu complete"
