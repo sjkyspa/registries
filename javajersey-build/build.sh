c7c842ea712224fc7d33464bd68937bb153ed768
jksun <sjkyspa@gmail.com>
2015-12-15 18:56:11 +0800

    chang build

--- javajersey-build/build.sh
+++ javajersey-build/build.sh
@@ -18,7 +18,7 @@ puts_step() {
   echo $'\033[0;34m'" -----> $@" $'\033[0m'
 }
 
-cd /tmp/repo/app
+cd /tmp/repo
 mysql=$(docker run -d -P -e MYSQL_USER=mysql -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=ke_tsu -e MYSQL_ROOT_PASSWORD=mysql hub.deepi.cn/mysql)
 if [ $? -ne 0 ]; then
     exit 1
