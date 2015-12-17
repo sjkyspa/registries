5431abde1a2ca4f75f0d0d5c7462782df9622e39
tw <tw@thoughtworks.com>
2015-12-17 13:06:58 +0800

    use rds

--- javajersey-build/build.sh
+++ javajersey-build/build.sh
@@ -63,55 +63,55 @@ puts_step "Generate standalone Complete"
 
 (cat  <<'EOF'
 #!/bin/sh
-host_ip=$(ip route|awk '/default/ { print $3 }')
-mysql_exists_code=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service/data|jq -r ".errorCode")
-if [ "$mysql_exists_code" == "" ] ; then
-        echo "etcd not started"
-        exit 1
-fi
-
-if [ "$mysql_exists_code" == "100" -o "$mysql_exists_code" == "null" ] ; then
-echo "launch backend service"
-cat >mysql.json <<EOFINNER
-{
-  "desiredState": "launched",
-  "options": [
-    {
-      "section": "Service",
-      "name": "ExecStartPre",
-      "value": "/bin/sh -c \"etcdctl set /services/%n/data 'user=mysql&password=mysql'\""
-    },
-    {
-      "section": "Service",
-      "name": "ExecStartPre",
-      "value": "/bin/sh -c \"docker inspect %n >/dev/null 2>&1 && docker rm -f %n || true\""
-    },
-    {
-      "section": "Service",
-      "name": "ExecStart",
-      "value": "/bin/sh -c \"docker run --name %n --rm -v /var/run/docker.sock:/var/run/docker.sock -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_USER=mysql -e SERVICE_NAME=%n -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=appdb -P  mysql\""
-    },
-    {
-      "section": "Service",
-      "name": "ExecStop",
-      "value": "/usr/bin/docker stop %n"
-    },
-    {
-      "section": "Service",
-      "name": "Restart",
-      "value": "on-failure"
-    }
-  ]
-}
-EOFINNER
-curl -X PUT -H "Content-Type:application/json" http://${host_ip}:49153/v1-alpha/units/${APP_NAME}_mysql.service -d @mysql.json
-sleep 20
-fi
-
-echo "find backend service"
-credential=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service/data|jq -r '.node.value')
-ipport=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service|jq -r '.node.nodes|.[]|select(.key != "/services/'"${APP_NAME}"'_mysql.service/data") |.value')
-export DATABASE="jdbc:mysql://${ipport}/appdb?${credential}"
+#host_ip=$(ip route|awk '/default/ { print $3 }')
+#mysql_exists_code=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service/data|jq -r ".errorCode")
+#if [ "$mysql_exists_code" == "" ] ; then
+#        echo "etcd not started"
+#        exit 1
+#fi
+#
+#if [ "$mysql_exists_code" == "100" -o "$mysql_exists_code" == "null" ] ; then
+#echo "launch backend service"
+#cat >mysql.json <<EOFINNER
+#{
+#  "desiredState": "launched",
+#  "options": [
+#    {
+#      "section": "Service",
+#      "name": "ExecStartPre",
+#      "value": "/bin/sh -c \"etcdctl set /services/%n/data 'user=mysql&password=mysql'\""
+#    },
+#    {
+#      "section": "Service",
+#      "name": "ExecStartPre",
+#      "value": "/bin/sh -c \"docker inspect %n >/dev/null 2>&1 && docker rm -f %n || true\""
+#    },
+#    {
+#      "section": "Service",
+#      "name": "ExecStart",
+#      "value": "/bin/sh -c \"docker run --name %n --rm -v /var/run/docker.sock:/var/run/docker.sock -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_USER=mysql -e SERVICE_NAME=%n -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=appdb -P  mysql\""
+#    },
+#    {
+#      "section": "Service",
+#      "name": "ExecStop",
+#      "value": "/usr/bin/docker stop %n"
+#    },
+#    {
+#      "section": "Service",
+#      "name": "Restart",
+#      "value": "on-failure"
+#    }
+#  ]
+#}
+#EOFINNER
+#curl -X PUT -H "Content-Type:application/json" http://${host_ip}:49153/v1-alpha/units/${APP_NAME}_mysql.service -d @mysql.json
+#sleep 20
+#fi
+#
+#echo "find backend service"
+#credential=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service/data|jq -r '.node.value')
+#ipport=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service|jq -r '.node.nodes|.[]|select(.key != "/services/'"${APP_NAME}"'_mysql.service/data") |.value')
+#export DATABASE="jdbc:mysql://${ipport}/appdb?${credential}"
 flyway migrate -url="$DATABASE" -locations=filesystem:`pwd`/dbmigration
 flyway migrate -url="$DATABASE" -locations=filesystem:`pwd`/initmigration -table="init_version" -baselineOnMigrate=true -baselineVersion=0
 java -jar app-standalone.jar
