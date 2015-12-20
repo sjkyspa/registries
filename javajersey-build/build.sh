124338188440537c8d42e6eefb0032924a181701
jksun <jksun@thoughtworks.com>
2015-12-20 21:41:54 +0800

    build the verify image

--- javajersey-build/build.sh
+++ javajersey-build/build.sh
@@ -78,55 +78,6 @@ puts_step "Generate standalone Complete"
 
 (cat  <<'EOF'
 #!/bin/sh
-#host_ip=$(ip route|awk '/default/ { print $3 }')
-#mysql_exists_code=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service/data|jq -r ".errorCode")
-#if [ "$mysql_exists_code" == "" ] ; then
-#        echo "etcd not started"
-#        exit 1
-#fi
-#
-#if [ "$mysql_exists_code" == "100" -o "$mysql_exists_code" == "null" ] ; then
-#echo "launch backend service"
-#cat >mysql.json <<EOFINNER
-#{
-#  "desiredState": "launched",
-#  "options": [
-#    {
-#      "section": "Service",
-#      "name": "ExecStartPre",
-#      "value": "/bin/sh -c \"etcdctl set /services/%n/data 'user=mysql&password=mysql'\""
-#    },
-#    {
-#      "section": "Service",
-#      "name": "ExecStartPre",
-#      "value": "/bin/sh -c \"docker inspect %n >/dev/null 2>&1 && docker rm -f %n || true\""
-#    },
-#    {
-#      "section": "Service",
-#      "name": "ExecStart",
-#      "value": "/bin/sh -c \"docker run --name %n --rm -v /var/run/docker.sock:/var/run/docker.sock -e MYSQL_ROOT_PASSWORD=mysql -e MYSQL_USER=mysql -e SERVICE_NAME=%n -e MYSQL_PASSWORD=mysql -e MYSQL_DATABASE=appdb -P  mysql\""
-#    },
-#    {
-#      "section": "Service",
-#      "name": "ExecStop",
-#      "value": "/usr/bin/docker stop %n"
-#    },
-#    {
-#      "section": "Service",
-#      "name": "Restart",
-#      "value": "on-failure"
-#    }
-#  ]
-#}
-#EOFINNER
-#curl -X PUT -H "Content-Type:application/json" http://${host_ip}:49153/v1-alpha/units/${APP_NAME}_mysql.service -d @mysql.json
-#sleep 20
-#fi
-#
-#echo "find backend service"
-#credential=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service/data|jq -r '.node.value')
-#ipport=$(curl -sL http://${host_ip}:4001/v2/keys/services/${APP_NAME}_mysql.service|jq -r '.node.nodes|.[]|select(.key != "/services/'"${APP_NAME}"'_mysql.service/data") |.value')
-#export DATABASE="jdbc:mysql://${ipport}/appdb?${credential}"
 flyway migrate -url="$DATABASE" -locations=filesystem:`pwd`/dbmigration
 flyway migrate -url="$DATABASE" -locations=filesystem:`pwd`/initmigration -table="init_version" -baselineOnMigrate=true -baselineVersion=0
 java -jar app-standalone.jar
@@ -135,13 +86,7 @@ EOF
 
 (cat << EOF
 FROM hub.deepi.cn/java
-RUN apk --update add tar bash
-ENV ETCD_VERSION 2.1.2
-RUN curl -jksSL https://github.com/coreos/etcd/releases/download/v\${ETCD_VERSION}/etcd-v\${ETCD_VERSION}-linux-amd64.tar.gz \
-	|   tar -xzf - -C /usr/local/bin/ --strip-components=1 && \
-	chmod +x /usr/local/bin/etcdctl
-RUN curl -jksSL https://github.com/stedolan/jq/releases/download/jq-1.5/jq-linux64 -o /usr/local/bin/jq && \
-	chmod +x /usr/local/bin/jq
+RUN apk --update add tar
 ADD src/main/resources/db/migration dbmigration
 ADD src/main/resources/db/init initmigration
 RUN mkdir /usr/local/bin/flyway && \
@@ -157,8 +102,22 @@ ENTRYPOINT ["./wrapper.sh"]
 EOF
 ) > Dockerfile
 
+(cat << EOF
+FROM hub.deepi.cn/java
+ADD build/libs/verify-standalone.jar verify-standalone.jar
+CMD ["java", "-jar", "verify-standalone.jar"]
+EOF
+) > Dockerfile.verify
+
 echo
 puts_step "Building image ..."
 docker build -t $IMAGE . &>process.log
 puts_step "Building image $IMAGE complete "
+echo
+
+echo
+puts_step "Building verify image ..."
+GRADLE_USER_HOME="$CACHE_DIR" gradle itestJar &>process.log
+docker build -f Dockerfile.verfiy -t $VERIFY_IMAGE . &>process.log
+puts_step "Building verify image $VERIFY_IMAGE complete"
 echo
