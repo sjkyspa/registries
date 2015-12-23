0f0fd562c971cadd4f376a5e7269220bfcaef2a4
jksun <jksun@thoughtworks.com>
2015-12-23 18:22:33 +0800

    add test for haproxy

--- haproxy/test/run-test.sh
+++ haproxy/test/run-test.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
+set -eo pipefail
 
-docker run -d --net=host --name etcd quay.io/coreos/etcd:v2.2.2
 docker build -t haproxy .
-docker run -d -e BACKEND=http://127.0.0.1:4001 haproxy
+HOST_IP=$(ip route|awk '/default/ { print $3 }')
+(docker stop etcd && docker rm etcd || true) &&  docker run -d --net=host --name etcd quay.io/coreos/etcd:v2.2.2
+docker run --net=host -e BACKEND=http://$HOST_IP:4001 haproxy
+
+until curl http://$HOST_IP:65535/ -v; do
+    echo "...."
+    sleep 1
+done
--- haproxy/test/test.sh
+++ haproxy/test/test.sh
@@ -4,7 +4,6 @@ set -eo pipefail
 BASEDIR=$(dirname $0)
 
 cd $BASEDIR
-cat ./Dockerfile
-docker build -f ./Dockerfile -t haproxytest ..
 
+docker build -f ./Dockerfile -t haproxytest ..
 docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -ti --rm haproxytest
