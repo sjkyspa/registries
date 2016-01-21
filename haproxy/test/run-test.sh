a8a3813a1541b68f57f8787932b192ea03df705d
jksun <sjkyspa@gmail.com>
2016-01-21 23:24:06 +0800

    init the haproxy

--- haproxy/test/run-test.sh
+++ haproxy/test/run-test.sh
@@ -32,19 +32,45 @@ HOST_IP=$(ip route|awk '/default/ { print $3 }')
 ETCD_CONTAINER=$( docker run -d -p 4001:4001 -p 2380:2380 -p 2379:2379 \
  --name etcd quay.io/coreos/etcd \
  -name etcd0 \
- -advertise-client-urls http://${HostIP}:2379,http://${HostIP}:4001 \
+ -advertise-client-urls http://${HostIP}:2379,http://${HOST_IP}:4001 \
  -listen-client-urls http://0.0.0.0:2379,http://0.0.0.0:4001 \
- -initial-advertise-peer-urls http://${HostIP}:2380 \
+ -initial-advertise-peer-urls http://${HOST_IP}:2380 \
  -listen-peer-urls http://0.0.0.0:2380 \
  -initial-cluster-token etcd-cluster-1 \
- -initial-cluster etcd0=http://${HostIP}:2380 \
+ -initial-cluster etcd0=http://${HOST_IP}:2380 \
  -initial-cluster-state new)
 
 HAPROXY_CONTAINER=$(docker run -d --net=host -e BACKEND=http://$HOST_IP:4001 haproxy)
 
-until curl http://$HOST_IP:65535/ -v; do
+until curl http://$HOST_IP:65535/ -v &>/dev/null; do
     echo "...."
     sleep 1
 done
 
 echo "haproxy initialized"
+
+etcd_set() {
+    if [ $# != 2 ]; then
+        echo "need two parameter"
+    fi
+    curl -XPUT -v http://${HOST_IP}:4001/v2/keys$1 -d value="$2"
+}
+
+etcd_get() {
+    if [ $# != 1 ]; then
+        echo "need one parameter"
+    fi
+    curl http://${HOST_IP}:4001/v2/keys$1
+}
+
+echo "init the etcd with stub config"
+etcd_set "/services-internal/mysql/ipport" "$HOST_IP:4001"
+
+echo "should wait and get access the of the port"
+
+until curl http://$HOST_IP:51000/ -v; do
+    etcd_get "/services-internal"
+    echo "...."
+    sleep 1
+    docker logs $HAPROXY_CONTAINER
+done
