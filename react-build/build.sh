ead85fda6fa5bc45a7569ee0cb855b46a4d268c1
xushanchuan <aisensiy@163.com>
2016-02-28 16:24:22 +0800

    cd codebase

--- react-build/build.sh
+++ react-build/build.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-cd /tmp/repo/
-
 puts_red() {
     echo $'\033[0;31m'"      $@" $'\033[0m'
 }
@@ -19,6 +17,32 @@ puts_step() {
   echo $'\033[0;34m'" -----> $@" $'\033[0m'
 }
 
+on_exit() {
+    last_status=$?
+    if [ "$last_status" != "0" ]; then
+        if [ -f "process.log" ]; then
+          cat process.log|puts_red_f
+        fi
+
+        exit 1;
+    else
+        if [ -n "$MYSQL_CONTAINER" ]; then
+            echo
+            puts_step "Cleaning ..."
+            docker stop $MYSQL_CONTAINER &>process.log && docker rm $MYSQL_CONTAINER &>process.log
+            puts_step "Cleaning complete"
+            echo
+        fi
+        puts_green "build success"
+        exit 0;
+    fi
+}
+
+trap on_exit HUP INT TERM QUIT ABRT EXIT
+
+CODEBASE_DIR=$CODEBASE
+
+cd $CODEBASE_DIR
 
 puts_step "Staring install depends ..."
 npm install
@@ -56,5 +80,5 @@ CMD ["nginx", "-g", "daemon off;"]
 EOF
 
 puts_step "Start building app image ..."
-docker build -t $IMAGE /tmp/repo
-puts_step "Building app image complete"
+docker build -t $IMAGE .
+puts_step "Building image $IMAGE complete"
