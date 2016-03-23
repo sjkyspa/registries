db3fe954403a4699e090ca32e04c82d6d63b5b65
sjkyspa <sjkyspa@gmail.com>
2016-03-23 10:08:28 +0800

    remove the paas related to stacks

--- 000-base/test/test.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -eo pipefail
-
-BASEDIR=$(dirname $0)
-
-cd $BASEDIR
-
-docker build -f ./Dockerfile -t haproxytest ..
-docker run --privileged -v /var/run/docker.sock:/var/run/docker.sock -ti --rm haproxytest
