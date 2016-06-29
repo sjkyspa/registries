5d5d5c158dcd3bb89690b7807337fb1e07f312d0
xushanchuan <aisensiy@163.com>
2016-06-29 18:11:13 +0800

    build update

--- javajersey-build/build.sh
+++ javajersey-build/build.sh
@@ -93,7 +93,7 @@ puts_step "Generate standalone Complete"
 (cat  <<'EOF'
 #!/bin/sh
 
-export DATABASE="jdbc:mysql://127.0.0.1:$DB_PORT/$DB_NAME?user=mysql&password=mysql&allowMultiQueries=true&zeroDateTimeBehavior=convertToNull&createDatabaseIfNotExist=true"
+export DATABASE="jdbc:mysql://$DB_HOST:$DB_PORT/$DB_NAME?user=mysql&password=mysql&allowMultiQueries=true&zeroDateTimeBehavior=convertToNull&createDatabaseIfNotExist=true"
 flyway migrate -url="$DATABASE" -locations=filesystem:`pwd`/dbmigration -baselineOnMigrate=true -baselineVersion=0
 [ -d `pwd`/initmigration  ] && flyway migrate -url="$DATABASE" -locations=filesystem:`pwd`/initmigration -table="init_version" -baselineOnMigrate=true -baselineVersion=0
 java -jar app-standalone.jar
